ef51c6123a922fe401cf3f79ca994d79fa77a459
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-01-04 15:48:59 +0900

    Time: 392 ms (5.22%), Space: 36.9 MB (19.90%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.kt
+++ 0088-merge-sorted-array/0088-merge-sorted-array.kt
@@ -1,29 +1,23 @@
 class Solution {
-    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
 
-        val numList = mutableListOf<Int>()
-    
-        if(nums1.isNotEmpty()){
-            for(index in 0 until m) {
-                numList.add(nums1[index])
-            }
+        val list = mutableListOf<Int>()
+
+
+
+        for(nums1Index in 0 until m) {
+            list.add(nums1[nums1Index])
         }
-
-        if(nums2.isNotEmpty()){
-            for(index in 0 until n) {
-                numList.add(nums2[index])
-            }
+        for(nums2Index in 0 until n) {
+            list.add(nums2[nums2Index])
         }
+        list.sort()
 
-        numList.sort()
-
-        for(temp in 0 until nums1.size) {
-            nums1[temp] = numList[temp]
-
+        for(temp in 0 until list.size) {
+            nums1[temp] = list[temp]
         }
 
 //        nums1.forEach { print(it) }
-
 
     }
 
