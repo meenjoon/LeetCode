8c573b0613a8ae9853c8b618d4ce21ffcedd7066
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-01-09 13:46:02 +0900

    Time: 402 ms (69.09%), Space: 41.3 MB (96.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.kt
+++ 0049-group-anagrams/0049-group-anagrams.kt
@@ -1,27 +1,27 @@
 class Solution {
-fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
 
-    val list = mutableListOf<MutableList<String>>()
+        val list = mutableListOf<MutableList<String>>()
 
-    val map = mutableMapOf<String,MutableList<String>>()
+        val map = mutableMapOf<String,MutableList<String>>()
 
-    for((index, value) in strs.withIndex()) {
+        for((index, value) in strs.withIndex()) {
 
-        val sorted = value.toCharArray().sorted().joinToString("")
+            val sorted = value.toCharArray().sorted().joinToString("")
 
-        if(map.containsKey(sorted)) { // 키 값이 이미 존재한다면
-            map.getValue(sorted).add(value)
-//            map[sorted]!!.add(value) 이것도 가능
+            if(map.containsKey(sorted)) { // 키 값이 이미 존재한다면
+                map.getValue(sorted).add(value)
+    //            map[sorted]!!.add(value) 이것도 가능
+            }
+            else { // 키 값이 존재하지 않는다면
+                map.put(sorted, mutableListOf(value))
+            }
+
         }
-        else { // 키 값이 존재하지 않는다면
-            map.put(sorted, mutableListOf(value))
-        }
+        map.forEach { list.add(it.value) } //map에 들어있는 MutableList<String>을 list에 넣는다.
+    //
+    //    list.forEach { print(it) }
 
+        return list
     }
-    map.forEach { list.add(it.value) } //map에 들어있는 MutableList<String>을 list에 넣는다.
-//
-//    list.forEach { print(it) }
-
-    return list
-}
 }
