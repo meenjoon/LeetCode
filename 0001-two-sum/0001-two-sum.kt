a6511f15c4a6249ef318c43d7153ecc1c16c57e8
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-01-13 11:58:08 +0900

    Time: 397 ms (44.47%), Space: 38.2 MB (64.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -5,15 +5,16 @@ class Solution {
 
         for(index in 0 until nums.size) {
             for(index2 in index until nums.size) {
+
                 if(index != index2 && nums[index] + nums[index2] == target) {
-    //                println("index : $index index2 : $index2")
                     list.add(index)
                     list.add(index2)
-                    break
                 }
             }
         }
-        // list.forEach { print(it) }
+    //    
+    //    list.forEach { print(it) }
+
 
         return list.toIntArray()
     }
