0d7fe51c2bb85d0954d95b9c38f6d0bda26ce3fe
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-01-28 16:30:53 +0900

    Time: 341 ms (32.24%), Space: 41.1 MB (29.39%) - LeetHub

--- 0046-permutations/0046-permutations.kt
+++ 0046-permutations/0046-permutations.kt
@@ -5,24 +5,44 @@ class Solution {
 
         fun dfs(start: Int){
             if(numList.size == nums.size) {
-                // println("//추가한 값, ${numList}")
+                // println("//추가한 값, ${numList} start: $start")
                 resultList.add(numList.toMutableList())
                 return
             }
 
-            for((index,value) in nums.withIndex()) {
-                if(!numList.contains(value)) {
-
-                    numList.add(value)
-                    // println("바로 추가, ${numList}")
+            for(index in 0 until nums.size) {
+                if(!numList.contains(nums[index])) {
+                    numList.add(nums[index])
+                    // println("바로 추가, $numList start: $start index: $index, value: ${nums[index]}")
 
                     dfs(index + 1)
-                    // println("빠져나옴, ${numList}")
+                    // println("빠져나옴, $numList start: $start index: $index, value: ${nums[index]}")
 
                     numList.removeAt(numList.size-1)
-                    // println("마지막 값 삭제, ${numList}")
+                    // println("마지막 값 삭제, $numList")
                 }
+
             }
+           
+            /*
+            아래의 방법도 가능하다.
+             */
+            
+//            for((index,value) in nums.withIndex()) {
+//                println("그냥 for문 무조건 실행, ${numList} start: $start index: $index, value: $value")
+//
+//                if(!numList.contains(value)) {
+//
+//                    numList.add(value)
+//                    println("바로 추가, ${numList} start: $start index: $index, value: $value")
+//
+//                    dfs(index + 1)
+//                    println("빠져나옴, ${numList} start: $start index: $index, value: $value")
+//
+//                    numList.removeAt(numList.size-1)
+//                    println("마지막 값 삭제, ${numList}")
+//                }
+//            }
         }
 
         dfs(0)
